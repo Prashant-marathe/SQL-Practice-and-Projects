7cbf2792c68b8990c3d41764853636304f36c0a4
Prashant-marathe <prashantmarathe150@gmail.com>
2026-05-19 17:30:17 +0530

    Basic Joins complete

---
+++ BasicJoins.sql
@@ -0,0 +1,71 @@
+-- Joins in SQL (Horizontal Combination)
+/* Joins require a common column (usually a Primary Key in one table)
+ and a foreign key in other to link records. */
+-- There are multiple times that differ based on Use case
+
+-- Basic Joins in SQL
+-- 1. NO JOIN: Returns data from both tables without combining them
+/* Retrive all data from customers and orders in 2 different result */
+SELECT * FROM customers;
+SELECT * FROM orders;
+
+-- 2. INNER JOIN: Returns only matching rows from both tables
+/* Get all customers along with their orders, but only for customers who have placed an order*/
+SELECT 
+	-- id, first_name, order_id, sales
+	
+	-- customers.id, customers.first_name, orders.order_id, orders.sales
+
+	c.id,
+	c.first_name,
+	o.order_id, 
+	o.sales
+FROM customers AS c
+INNER JOIN orders AS o
+-- ON customers.id = orders.customer_id
+ON c.id = o.customer_id
+-- ON id = customer_id
+
+-- LEFT JOIN: Get all the rows from the left and Only the matching from the right
+/* Get all customers along with their orders, including those withour orders */
+SELECT 
+	c.id,
+	c.first_name,
+	o.order_id,
+	o.sales
+FROM customers AS c
+LEFT JOIN orders AS o
+ON c.id = o.customer_id
+
+-- RIGHT JOIN: Get all the rows from the right and only the matching rows from the left
+/* Get all customers along with their orders, including orders without matching customers */
+SELECT 
+	c.id,
+	c.first_name,
+	o.order_id,
+	o.sales
+FROM customers AS c
+RIGHT JOIN orders AS o
+ON c.id = o.customer_id
+
+/* Get all customers along with their orders,
+including orders without matching customers (Using LEFT JOIN) */
+SELECT 
+	c.id, 
+	c.first_name,
+	o.order_id,
+	o.sales
+FROM orders AS o
+LEFT JOIN customers AS c
+ON o.customer_id = c.id
+
+-- FULL JOIN / OUTER JOIN: Returns everything from both tables
+/* Get all customers and all orders, even if there's no match.*/
+SELECT 
+	c.id,
+	c.first_name,
+	o.order_id,
+	o.sales
+FROM customers AS c
+FULL JOIN orders AS o
+ON c.id = o.customer_id
--- operators.sql
+++ operators.sql
@@ -43,3 +43,68 @@ WHERE country = 'USA' OR score > 500
 -- (NOT) - Reverse the condition. True becomes False and False becomes True. Excludes the matching row.
 SELECT * FROM customers
 WHERE NOT(country = 'USA' AND score > 500)
+
+-- Range Operator 
+-- (BETWEEN) - Checks if values are between an certain range. Marks Lower Boundary and Upper Boundary and anything inside this boundaries is true, anything outside is false. This boundaries are inclusive
+SELECT * 
+FROM customers 
+WHERE score BETWEEN 100 AND 500
+
+SELECT * 
+FROM customers
+WHERE (score >= 100 and score <= 500)
+
+
+-- Membership Operator
+-- (IN) : Check if a value exists in a list
+SELECT * 
+FROM customers
+WHERE country IN ('Germany', 'USA')
+
+-- (NOT IN): Opposite of IN. Check if the value does not exist in the list and returns that value
+SELECT * 
+FROM customers
+WHERE country NOT IN ('Germany', 'USA')
+
+
+-- Search Operator
+-- (LIKE) : Search for a pattern in a text
+-- % - Means anything, (_) - Exactly one value
+-- M% : Must start with M and anything after that
+-- %in : Must end with in and anything after that
+-- %r% : Must have an 'r' somewhere in the text
+-- __b% : We need an char at first and second position and then third character is b and anything after that is not we care about. eg. Albert, Rob, Abel
+
+
+-- Find all customers whose first starts with an capital 'M'
+
+SELECT * FROM customers
+WHERE first_name LIKE 'M%'
+
+-- Find all customers whose first name ends with 'n'
+SELECT * 
+FROM customers
+WHERE first_name LIKE '%n'
+
+
+-- Find customers whose first name contains an 'r'
+SELECT * FROM customers
+WHERE first_name LIKE '%r%'
+
+
+-- find customers whose first_name has an 'r' in the third position
+SELECT * FROM customers
+WHERE first_name LIKE '__r%'
+
+
+-- Handling Missing Data (`IS NULL`)
+/* An important topic to add to your filtering toolkit is handling **missing or unentered data**. As we saw briefly in the DML section, `NULL` cannot be evaluated with standard comparison operators like `= NULL` or `!= NULL`. Instead, SQL provides specific operators for this: */
+
+
+-- Find customers who don't have a score registered yet
+SELECT * FROM customers 
+WHERE score IS NULL;
+
+-- Find customers who have any score registered
+SELECT * FROM customers 
+WHERE score IS NOT NULL;
